66dfcf1eb14daf5c3ce2ae5a00b58efba1ae1efb
fmueller <frank.mueller@kisters.de>
2016-11-18 13:21:57 +0100

    init 3

---
+++ FunkNode/SSID.h
@@ -0,0 +1,6 @@
+const char* ssid = "yourssid";
+const char* password = "yourpassword";
+const char* mqttClientName = "yourArduinoClient";
+const char* mqttUserName = "yourmqttuser";
+const char* mqttPassword = "yourmqttPassword";
+ 
